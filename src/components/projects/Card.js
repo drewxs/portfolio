274b9fd0ac7d14da8fba73c479d59de288ab8fca
kito0 <drewshah0@gmail.com>
2021-05-05 22:52:50 -0600

    Add Project page and Card component

--- src/components/projects/Card.js
+++ src/components/projects/Card.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './css/card.css';
 
-export default function Card() {
+export default function Card({ title, body, link }) {
 	return (
-		<div className="card">
-			<h2 className="card-title">project name</h2>
-			<p className="card-body">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-				explicabo, dolore sapiente eum voluptatem corporis natus eaque
-				consequuntur enim rem?
-			</p>
-			<a href="#" className="button">
-				Learn More
-			</a>
+		<div
+			className={`card ${title
+				.toLowerCase()
+				.replace(/\s/g, '')
+				.replace(/[()]/g, '')}`}
+		>
+			<div className="card-content">
+				<h2 className="card-title">{title}</h2>
+				<p className="card-body">{body}</p>
+				<a href={link} target="_blank" rel="noreferrer" className="button">
+					Learn More
+				</a>
+			</div>
 		</div>
 	);
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,9 +5,50 @@ import Card from './Card';
 export default function Projects() {
 	return (
 		<div className="projects">
-			<h2>Projects</h2>
-			<Card />
-			<Card />
+			<div className="project-title">
+				{window.matchMedia('(min-width: 420px)').matches ? (
+					<h2>Here are some of my projects</h2>
+				) : (
+					<h2>Projects</h2>
+				)}
+			</div>
+			<div className="project-list">
+				<Card
+					title="Raven"
+					body="A social media platform"
+					link="https://ravenx.vercel.app"
+				/>
+				<Card
+					title="Ciel"
+					body="A ERC-20 Standard Cryptocurrency"
+					link="https://github.com/kito0/ciel-token"
+				/>
+				<Card
+					title="Digit Classification"
+					body="ANN for digit classification"
+					link="https://github.com/kito0/digit-classification"
+				/>
+				<Card
+					title="Object Detection"
+					body="Object Detection with Tensorflow"
+					link="https://github.com/kito0/object-detection"
+				/>
+				<Card
+					title="Zapr"
+					body="A blogging web app"
+					link="https://zapr.netlify.app"
+				/>
+				<Card
+					title="Plannr"
+					body="Personal Task Planner"
+					link="https://github.com/kito0/plannr"
+				/>
+				<Card
+					title="Raven (Unsupported)"
+					body="Older rendition of Raven"
+					link="https://ravenx.netlify.app"
+				/>
+			</div>
 		</div>
 	);
 }

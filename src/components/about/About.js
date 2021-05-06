796d28cc8c67e795411902050b36e9c6fe7b05f7
kito0 <drewshah0@gmail.com>
2021-05-06 08:36:39 -0600

    Add GitHub link

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,19 +2,33 @@ import React from 'react';
 import './css/about.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { IconButton } from '@material-ui/core';
+import { GitHub } from '@material-ui/icons';
 
 export default function About() {
 	AOS.init();
 
 	return (
 		<div className="about">
-			<h2>About Me</h2>
-			<h6>
+			<h2 className="about-title">About Me</h2>
+			<h6 className="about-body">
 				Hi, I like to code and make stuff. My favourite tech stack currently is:
 				MongoDB, Express, React, Node, Redux. I like to constantly learn new
 				technology and challenge myself. I also love to collaborate with people.
 				Have a project idea and want to make something together? Hit me up!
+				<br />
+				<br />
+				email: drewshah0@gmail.com
 			</h6>
+			<IconButton
+				href="https://github.com/kito0"
+				target="_blank"
+				rel="noreferrer"
+				color="secondary"
+				className="github"
+			>
+				<GitHub style={{ width: '64px', height: '64px' }} />
+			</IconButton>
 		</div>
 	);
 }

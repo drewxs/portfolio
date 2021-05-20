621baf10fe27775670ce65b3ab995d3b408f8d09
kito0 <drewshah0@gmail.com>
2021-05-20 14:47:13 -0600

    Update contact info in about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,7 @@ import './css/about.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { IconButton } from '@material-ui/core';
-import { GitHub } from '@material-ui/icons';
+import { GitHub, Email, Call } from '@material-ui/icons';
 import Typed from 'react-typed';
 
 export default function About() {
@@ -18,19 +18,34 @@ export default function About() {
 				Hi, I like to code and make stuff. My favourite tech stack currently is:
 				MongoDB, Express, React, Node, Redux. I like to constantly learn new
 				technology and challenge myself. I also love to collaborate with people.
-				Have a project idea and want to make something together? Hit me up!
+				Have a project idea and want to make something together? Contact me!
 				<br />
 				<br />
-				email: drewshah0@gmail.com
 			</h6>
 			<IconButton
 				href="https://github.com/kito0"
 				target="_blank"
 				rel="noreferrer"
 				color="secondary"
-				className="github"
+				className="about-btn"
 			>
-				<GitHub style={{ width: '64px', height: '64px' }} />
+				<GitHub style={{ width: '5rem', height: '5rem' }} />
+			</IconButton>
+			<IconButton
+				href={'mailto: drewshah0@gmail.com'}
+				rel="noreferrer"
+				color="secondary"
+				className="about-btn"
+			>
+				<Email style={{ width: '5rem', height: '5rem' }} />
+			</IconButton>
+			<IconButton
+				href={'tel: +15064610871'}
+				rel="noreferrer"
+				color="secondary"
+				className="about-btn"
+			>
+				<Call style={{ width: '5rem', height: '5rem' }} />
 			</IconButton>
 		</div>
 	);

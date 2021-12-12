5f95a5f3a01c7bc0bc6d44d05fadada3722bdc80
kito0 <drewshah0@gmail.com>
2021-12-11 18:50:25 -0700

    Update Services

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -33,31 +33,23 @@ export default function Services() {
 					<div className="skill-card-text">
 						<h4 className="skill-card-title">Backend</h4>
 						{window.matchMedia('(min-width: 768px)').matches && (
-							<div>
-								<br />
-								<h5 className="skill-card-description">
-									RESTful APIs and database design
-								</h5>
-								<br />
-								<br />
-								<h5 className="skill-card-description">
-									Languages/tools:
-								</h5>
-							</div>
+							<h5 className="skill-card-description">
+								RESTful APIs and database design
+							</h5>
 						)}
-						<h6 className="skill-card-list">
-							Node.js <br />
-							Express.js <br />
-							Ruby on Rails <br />
-							Flask <br />
-							Django <br />
-							Amazon S3 <br />
-							MongoDB <br />
-							Firebase <br />
-							PostgreSQL <br />
-							MySQL <br />
-							OracleDB
-						</h6>
+						<div className="skill-card-list">
+							<p>Node.js</p>
+							<p>Express.js</p>
+							<p>Ruby on Rails</p>
+							<p>Flask</p>
+							<p>Django</p>
+							<p>Amazon S3</p>
+							<p>MongoDB</p>
+							<p>Firebase</p>
+							<p>PostgreSQL</p>
+							<p>MySQL</p>
+							<p>OracleDB</p>
+						</div>
 					</div>
 				</Card>
 				<Card className="skill-card">
@@ -69,73 +61,53 @@ export default function Services() {
 					<div className="skill-card-text">
 						<h4 className="skill-card-title">Frontend</h4>
 						{window.matchMedia('(min-width: 768px)').matches && (
-							<div>
-								<br />
-								<h5 className="skill-card-description">
-									Responsive and interactive user interfaces
-								</h5>
-								<br />
-								<br />
-								<h5 className="skill-card-description">
-									Languages/tools:
-								</h5>
-							</div>
+							<h5 className="skill-card-description">
+								Responsive and interactive user interfaces
+							</h5>
 						)}
-						<h6 className="skill-card-list">
-							React.js <br />
-							Next.js <br />
-							Redux <br />
-							HTML <br />
-							CSS <br />
-							Sass <br />
-							Material-UI <br />
-							Tailwind <br />
-							Bootstrap <br />
-							Socket.io <br />
-							Pusher <br />
-						</h6>
+						<div className="skill-card-list">
+							<p>React.js</p>
+							<p>Next.js</p>
+							<p>Redux</p>
+							<p>HTML</p>
+							<p>CSS</p>
+							<p>Sass</p>
+							<p>Material-UI</p>
+							<p>Tailwind</p>
+							<p>Bootstrap</p>
+							<p>Socket.io</p>
+							<p>Pusher</p>
+						</div>
 					</div>
 				</Card>
-				{window.matchMedia('(min-width: 512px)').matches && (
-					<Card className="skill-card">
-						<CallMade
-							color="secondary"
-							className="skill-card-icon"
-							fontSize="large"
-						/>
-						<div className="skill-card-text">
-							<h4 className="skill-card-title">DevOps</h4>
-							{window.matchMedia('(min-width: 768px)')
-								.matches && (
-								<div>
-									<br />
-									<h5 className="skill-card-description">
-										Integration, testing, scaling, and
-										deploying
-									</h5>
-									<br />
-									<br />
-									<h5 className="skill-card-description">
-										Tools:
-									</h5>
-								</div>
-							)}
-							<h6 className="skill-card-list">
-								Git <br />
-								CircleCI <br />
-								Storybook <br />
-								Jest <br />
-								Mocha <br />
-								AVA <br />
-								Kubernetes <br />
-								Docker <br />
-								Netlify <br />
-								Vercel <br />
-								Heroku
-							</h6>
+				<Card className="skill-card">
+					<CallMade
+						color="secondary"
+						className="skill-card-icon"
+						fontSize="large"
+					/>
+					<div className="skill-card-text">
+						<h4 className="skill-card-title">DevOps</h4>
+						{window.matchMedia('(min-width: 768px)').matches && (
+							<h5 className="skill-card-description">
+								Integration, testing, scaling, and deploying
+							</h5>
+						)}
+						<div className="skill-card-list">
+							<p>Git</p>
+							<p>CircleCI</p>
+							<p>Storybook</p>
+							<p>Jest</p>
+							<p>Mocha</p>
+							<p>AVA</p>
+							<p>Kubernetes</p>
+							<p>Docker</p>
+							<p>Netlify</p>
+							<p>Vercel</p>
+							<p>Heroku</p>
 						</div>
-					</Card>
-				)}
+					</div>
+				</Card>
 			</div>
 		</div>
 	);

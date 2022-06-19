5aeaa094c9df6f95d766a7c81ed12785f41b8cac
kito0 <drewshah0@gmail.com>
2022-06-19 04:16:09 -0600

    feat: update nav styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,15 +2,15 @@ import { ThemeChanger } from 'components';
 
 export const Navbar = () => {
   return (
-    <section className='nav'>
+    <nav className='nav'>
       <div className='container nav__container'>
         <div className='brand'>
-          <h4>Navbar</h4>
+          <h4>Andrew Shah</h4>
         </div>
         <div className='nav__links'>
           <ThemeChanger />
         </div>
       </div>
-    </section>
+    </nav>
   );
 };

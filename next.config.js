0c685ea61472351dd4119622e1984db01c53cb0d
kito0 <drewshah0@gmail.com>
2022-06-19 19:11:55 -0600

    chore: update ts eslint configurations
    Specify directories for next eslint.
    Include all .d.ts files.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    dirs: ['components', 'layout', 'pages', 'styles'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

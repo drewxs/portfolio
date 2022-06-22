3bce39dd43d6f478918f35f1d5559d26a7519324
kito0 <drewshah0@gmail.com>
2022-06-21 23:21:22 -0600

    feat: create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initalProps = await Document.getInitialProps(ctx);
+
+    return initalProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

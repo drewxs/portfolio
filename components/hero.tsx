07985a2f3bd8744538c474ce6c8d43e2c5aa0e66
kito0 <drewshah0@gmail.com>
2022-06-22 20:39:54 -0600

    feat: update typewriter config

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,7 +9,10 @@ export const Hero = (): ReactElement => {
         <h1>
           <Typewriter text='Hi there,' />
           <br />
-          <Typewriter text='welcome to my site :)' delay={2000} linger />
+          <Typewriter text='welcome to my site :)' delay={2200} lingerDuration={0} />
+          <br />
+          <Typewriter text=' ' delay={3400} linger />
+          <br />
         </h1>
       </div>
     </section>

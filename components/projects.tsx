f22964348f4b36ca1087a0bbe8f1fa9a59db5805
kito0 <drewshah0@gmail.com>
2022-06-24 00:21:04 -0600

    feat: create projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,5 +1,26 @@
+import { Endpoints } from '@octokit/types';
 import { ReactElement } from 'react';
 
-export const Projects = (): ReactElement => {
-  return <></>;
+import { RepoCard } from 'components';
+
+type ReposType = Endpoints['GET /user/repos']['response']['data'];
+
+/**
+ * Section for showcasing featured projects.
+ *
+ * @param projects - Array of GitHub repository objects
+ */
+export const Projects = ({ repos }: { repos: ReposType }): ReactElement => {
+  return (
+    <section className='home code'>
+      <div className='container'>
+        <h1>Projects</h1>
+        <div className='repo-list'>
+          {repos?.map((repo) => {
+            return <RepoCard repo={repo} key={repo.id} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };

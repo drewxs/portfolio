646726e50b999d2369c5123028f8f9f9e035ec74
kito0 <drewshah0@gmail.com>
2022-06-26 12:07:22 -0600

    ci: update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,9 @@
 module.exports = {
   branches: ['master', 'version-2'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    ['@semantic-release/git', { assets: ['CHANGELOG.md'] }],
+  ],
 };

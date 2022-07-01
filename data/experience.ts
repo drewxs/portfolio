bff3922c677564ed74636096f64ff618d4af4959
kito0 <drewshah0@gmail.com>
2022-07-01 00:49:32 -0600

    feat: create experience.ts

---
+++ data/experience.ts
@@ -0,0 +1,14 @@
+export const experience = [
+  {
+    title: `Intermediate Developer`,
+    company: `Paper Crane`,
+    year: 2022,
+    url: `https://www.papercrane.ca`,
+  },
+  {
+    title: `Jr. Frontend Developer`,
+    company: `Paper Crane`,
+    year: 2021,
+    url: `https://www.papercrane.ca`,
+  },
+];

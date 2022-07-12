43666c34fd4a935e1fe7d2d5739ee9def74e59f0
kito0 <drewshah0@gmail.com>
2022-07-11 19:39:12 -0600

    feat: add link data

--- data/index.ts
+++ data/index.ts
@@ -2,3 +2,5 @@ export { contact } from 'data/contact';
 export { experience } from 'data/experience';
 export { hero } from 'data/hero';
 export { projects } from 'data/projects';
+export { nav } from 'data/links';
+export { socials } from 'data/links';
---
+++ data/links.ts
@@ -0,0 +1,7 @@
+export const socials = {
+	github: 'https://github.com/kito0',
+	linkedin: 'https://www.linkedin.com/in/andrewxshah',
+	medium: 'https://medium.com/@drewxs',
+};
+
+export const nav = [];

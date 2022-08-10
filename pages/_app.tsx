d79a008bdccb0325b916caea2b94f4eeb65c5eb7
kito0 <drewshah0@gmail.com>
2022-08-09 20:28:40 -0600

    feat: add google-analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,30 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import 'styles/_main.scss';
 
 import { Layout } from 'layout';
 
+const GTAG_ID = process.env.NEXT_PUBLIC_GTAG;
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider defaultTheme='system'>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`} strategy='afterInteractive' />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GTAG_ID}');
+        `}
+      </Script>
+      <ThemeProvider defaultTheme='system'>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 };
 

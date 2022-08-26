b307c669a5e91402dfa3b319c30134a1956d3b31
kito0 <drewshah0@gmail.com>
2022-08-25 21:10:30 -0600

    build: update minimumCacheTTL to 1 day

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    minimumCacheTTL: 86400,
+  },
   eslint: {
     dirs: ['components', 'layout', 'pages', 'styles'],
   },

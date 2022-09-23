30aea59846992ff0d46d985440c8cffabf75cd41
kito0 <drewshah0@gmail.com>
2022-09-22 20:57:42 -0600

    feat: update experience

--- data/experience.ts
+++ data/experience.ts
@@ -2,16 +2,21 @@ export const experience = [
   {
     _id: '02',
     year: 2022,
-    title: `Intermediate Developer`,
-    description: `Architected and drafted proposals for software products including REST APIs and web applications. Managed AWS services, CI/CD and databases. Collaborated with stakeholders to build products meeting company standards.`,
+    title: `Intermediate Full Stack Developer`,
+    description: `Architected and drafted proposals for various software products including APIs, mobile and web applications with a focus on cloud and microservice-based architectures.
+                  Facilitated deployments and environments through well-defined CI/CD pipelines.
+                  Collaborated with stakeholders to build products that go above satisfying the base requirements and meeting company and industry standards.`,
     company: `Paper Crane`,
     url: `https://www.papercrane.ca`,
   },
   {
     _id: '01',
     year: 2021,
-    title: `Jr. Frontend Developer`,
-    description: `Developed and maintained various full-stack applications using the latest technologies and frameworks. Analyzed and optimized performance bottlenecks in existing systems.`,
+    title: `Junior Frontend Developer`,
+    description: `Collaborated with designers to translate XD designs into reusable frontend components.
+                  Translated business requirements from clients to technical solutions.
+                  Migrated web applications to new tech stacks while maintaining existing features as well as adding new ones.
+                  Analyzed and optimized performance bottlenecks in existing systems.`,
     company: `Paper Crane`,
     url: `https://www.papercrane.ca`,
   },

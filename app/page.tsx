d7d9824724bdcd4b10810971108e9ac2a7b39c01
Andrew X. Shah <drewshah0@gmail.com>
2023-02-02 20:17:24 -0700

    fix: render code segment only if fetch succeeds

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import { Code, Contact, Experience, Hero, Projects } from 'components';
 
+const getRepos = async () => {
+  try {
+    const res = await fetch(`${process.env.SITE_URL}/api/repos`, { next: { revalidate: 60 } });
+    const data = await res.json();
+    return data.repos;
+  } catch (error) {
+    return error;
+  }
+};
+
 const Home = async () => {
-  const res = await fetch(`${process.env.SITE_URL}/api/repos`, { next: { revalidate: 60 } });
-  const data = await res.json();
+  const repos = await getRepos();
 
   return (
     <>
       <Hero />
-      <Code repos={data.repos} />
+      {!(repos instanceof Error) && <Code repos={repos} />}
       <Projects />
       <Experience />
       <Contact />

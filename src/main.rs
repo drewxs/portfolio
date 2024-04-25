d7f7796fecb74d73dcf987a672e53c5d1bd5d9c0
Andrew X. Shah <drewshah0@gmail.com>
2024-04-25 06:17:41 -0600

    build(logging): update dioxus logger

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use log::Level;
+use tracing::Level;
 
 use portfolio::route::Route;
 
 fn main() {
-    dioxus_logger::init(Level::Info).expect("failed to init logger");
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
     launch(App);
 }
 

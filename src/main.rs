5a76430e5db8fd231f9c4ac8541d31787daa4785
Andrew X. Shah <drewshah0@gmail.com>
2024-06-02 23:11:38 -0600

    chore: add font / base styles

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use tracing::Level;
 
 use portfolio::route::Route;
 
-const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
+const _TAILWIND_STYLESHEET: &str = manganis::mg!(file("./public/tailwind.css"));
+const _GLOBAL_STYLESHEET: &str = manganis::mg!(file("./public/main.css"));
+const _RESET_STYLESHEET: &str = manganis::mg!(file("./public/reset.css"));
+const _FONT: &str = manganis::mg!(font().families(["Source Code Pro"]).weights([400]));
 
 fn main() {
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
@@ -13,5 +16,7 @@ fn main() {
 }
 
 fn App() -> Element {
-    rsx! { Router::<Route> {} }
+    rsx! {
+        Router::<Route> {}
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   mode: "all",
   content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Source Code Pro", "monospace"],
+      body: ["Source Code Pro", "monospace"],
+    },
   },
   plugins: [],
 };
